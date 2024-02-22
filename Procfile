a95f517bca1601bf7c87003fca41dbccf5d686f3
CaseNightmareGreen <132308533+CaseNightmareGreen@users.noreply.github.com>
2024-02-21 17:14:33 -0800

    With Procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn main.get_time
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/<time>/')
+@app.route('/')
 def get_time():
     current_time = int(time.time())
     return f'The current Epoch time is: {current_time}'
