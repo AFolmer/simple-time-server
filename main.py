cb2c83c5728b5253ebc7e37a9048b83a05f7493f
CaseNightmareGreen <132308533+CaseNightmareGreen@users.noreply.github.com>
2024-02-22 08:56:01 -0800

    Fixed favicon error

--- main.py
+++ main.py
@@ -12,5 +12,10 @@ def get_time():
     return f'The current Epoch time is: {current_time}'
 
 
+@app.route('/favicon.ico')
+def favicon():
+    return '', 204
+
+
 if __name__ == "__main__":
     app.run(debug=True)
